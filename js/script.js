c035c0b93c657b807f50533e9ca3fa78c1540baf
dargenaro <dargenaro@gmail.com>
2022-05-26 19:03:08 -0500

    primer trabajo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function nombreProfesora(){
+
+    var nombreProfesora = prompt ("Hola, profe. ¿Cómo está?");
+
+    if (nombreProfesora == "bien") {
+        alert("Me alegra mucho. Dado que no tengo usuario en algunas de las redes sociales que aparecen en el footer, direccioné los links a las páginas de inicio de las redes sociales, de modo que quedaran funcionales y así dar cumplimiento a los requerimientos de la actividad. Muchísimas gracias.")
+    } else {
+        alert("Nunca dejes de creer");
+
+}}
